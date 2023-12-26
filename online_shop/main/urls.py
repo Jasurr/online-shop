0db2d92ba95dc8b431d34b79b10984ffd61a6eba
jasur <njasur.1995@gmail.com>
2023-12-26 23:27:02 +0500

    The main page is ready, pages index and about

--- online_shop/main/urls.py
+++ online_shop/main/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
 
-from .views import index
+from . import views
+
+app_name = 'main'
 
 urlpatterns = [
-    path("/", index, name="home"),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+
 ]
--- online_shop/main/views.py
+++ online_shop/main/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the")
+    context = {
+        'title': 'Home',
+        'content': 'Магазин мебели HOME'
+    }
+    return render(request, 'main/index.html', context)
+def about(request):
+    context = {
+        'title': 'About',
+        'content': 'About us',
+        'text_on_page': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do'
+    }
+    return render(request, 'main/about.html', context)
