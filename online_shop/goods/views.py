8005575087840cba18a745d0695075393de929b5
jasur <njasur.1995@gmail.com>
2024-01-11 00:10:53 +0500

    added pagination

--- online_shop/goods/views.py
+++ online_shop/goods/views.py
@@ -1,21 +1,22 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
 
 
-def catalog(request, catalog_slug):
+def catalog(request, catalog_slug, page=1):
 
     if catalog_slug == 'all':
         goods = Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=catalog_slug))
+        goods = get_list_or_404(Products.objects.filter(category__slug=catalog_slug))
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(1)
+    current_page = paginator.page(page)
 
     context = {
         'title': 'Home - Katalog',
-        'goods': current_page
+        'goods': current_page,
+        'slug_url': catalog_slug
     }
     return render(request, 'goods/catalog.html', context)
 
