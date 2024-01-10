cb3dba911341a9b2ca07c85e002fd6fadf17d709
jasur <njasur.1995@gmail.com>
2024-01-10 22:57:18 +0500

    registered simple tag categories

--- online_shop/main/views.py
+++ online_shop/main/views.py
@@ -1,16 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-from goods.models import Categories
-
-
 def index(request):
-    categories = Categories.objects.all()
-
     context = {
         'title': 'Home',
         'content': 'Магазин мебели HOME',
-        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
